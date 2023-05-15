ba8b4509a16d92e71eab76bf55ad87b92db987be
Kai Jane <120575219+Kai-Denzel-Jane@users.noreply.github.com>
2023-05-15 20:13:23 +0800

    Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-wordle
 This is a repository for my Cert III Introduction to Programming project, which is making Worlde the NYT owned web game.
+
+@Kai-Denzel-Jane
--- main.py
+++ main.py
@@ -7,48 +7,39 @@ VALID_WORDS = pathlib.Path('./word-bank/all_words.txt')
 
 MAX_TRIES = 6
 
-guessed = []
-present_letters = []
+def files():
+    if os.path.exists(TARGET_WORDS):
+        with open(TARGET_WORDS, "r") as words:
+            target_words_contents = words.read().splitlines()
 
 
-if os.path.exists(TARGET_WORDS):
-    with open(TARGET_WORDS, "r") as words:
-        contents = words.read().splitlines()
+    if os.path.exists(VALID_WORDS):
+        with open(VALID_WORDS, "r") as valid_words:
+            valid_words_contents = valid_words.read().splitlines()
 
-    word = str(random.choice(contents))
+    return(target_words_contents, valid_words_contents)
+def selectword():
+
+    word = str(random.choice(files()[0]))
     print(word)
-
-if os.path.exists(VALID_WORDS):
-    with open(VALID_WORDS, "r") as valid_words:
-        valid_words_contents = valid_words.read().splitlines()
+selectword()
 
 def algorithm():
 
-    guessed.append(user_input)
-    
-    user_input_list = list(user_input)
-    word_list = list(word)
- 
+    print(comments)
+
 def userinput():
-    
-    if user_input in valid_words_contents:
-    
-        if len(user_input) == 5:
 
-            algorithm()
+    user_input = str(input("Enter a 5 letter word: "))
 
-            return(user_input)
+    if len(user_input) >> 5:
+        print("Maxium of 5 letters try again.")
+        userinput()
+    elif user_input in files()[1]:
+        algorithm()
+        return(user_input)
+    else:
+        print("-1")
     
-user_input = str(input("Enter a word: "))
 userinput()
 
-def attempts():
-    while MAX_TRIES > 0:
-        userinput()
-
-# TODO: ensure guess in VALID_WORDS
-
-# TODO: provide clues for each character in the guess using your scoring algorithm
-
-# (end loop)
-print("Game Over")
