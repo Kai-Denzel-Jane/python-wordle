7082ca59e5b5bce44f7ef5fa41424a0adbf3f690
yeetymeaty20 <120575219+yeetymeaty20@users.noreply.github.com>
2023-06-09 16:34:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
-# Import modules 
-import random # Used to generate random word 
-import pathlib # Used to define file paths because pythons default method can be weird
-import debug_module # File with debug functions 
-import base64 # Used to endcode / decode strings
-import options_module # File with option functions to allow user to change 
-import client                                       # File which asks for information and attempts to upload scores to server
-import yaml                                         # Downloaded package allowing for configuration to be saved in yaml files
-from colorama import Fore, Back, Style              # Downloaded package allowing for prettier terminal 
+import random
+import pathlib
+import debug_module
+import base64
+import options_module
+import client
+import yaml
+from colorama import Fore, Style, init
+import subprocess
 
-# Loads the configuration file
+# Initialize colorama
+init()
+
+# Load the configuration file
 with open("config.yaml", "r") as config_file:
-        config = yaml.safe_load(config_file)
+    config = yaml.safe_load(config_file)
 
-# Main Menu 
+# Main menu
 def welcome(cheat):
-    print(Fore.CYAN + "1. Play the game")           # Loads the game itself 
-    print("2. Instructions")                        # Opens Instructions.md with default set app
-    print("3. Debug")                               # Allows users to see debug information 
-    print("4. Options")                             # Where users can change options 
-    print("5. Credits / Information")               # "Just" Credits ;)
-    print("6. Exit")                                # Exits the script 
+    print(Fore.CYAN + "1. Play the game")                       # Loads the game itself
+    print("2. Instructions")                                    # Opens Instructions.md with default set app
+    print("3. Debug")                                           # Allows users to see debug information
+    print("4. Options")                                         # Where users can change options
+    print("5. Credits / Information")                           # "Just" Credits ;)
+    print("6. Exit")                                            # Exits the script
     print(Style.RESET_ALL)
 
     choice = int(input("Input number: "))
     return choice
 
-TARGET_WORDS = pathlib.Path('./word-bank/target_words.txt')         # Words that can possibly be chosen as the word yoh are guessing 
-VALID_WORDS = pathlib.Path('./word-bank/all_words.txt')             # Words that can be guessed 
+TARGET_WORDS = pathlib.Path('./word-bank/target_words.txt')     # Words that can possibly be chosen as the word you are guessing
+VALID_WORDS = pathlib.Path('./word-bank/all_words.txt')         # Words that can be guessed
 
-MAX_TRIES = 6                                                       # Amount of lives user has doesn't really need to be a constant 
+MAX_TRIES = 6                                                   # Amount of lives user has (doesn't really need to be a constant)
 
-# Loads necessary files
+# Load necessary files
 def load_files():
-    
     target_words_contents = []
     valid_words_contents = []
 
     if TARGET_WORDS.exists():
         with open(TARGET_WORDS, "r") as words:
             target_words_contents = words.read().splitlines()
-    
 
     if VALID_WORDS.exists():
         with open(VALID_WORDS, "r") as valid_words:
@@ -53,74 +54,75 @@ def select_word():
     word = random.choice(target_words_contents)
     return word
 
-# Main logic, this is where the scoring is implemented 
+# Main logic, implements the scoring
 def algorithm(user_word, word, tries, config, cheat):
-    user_word = list(user_word)                     # Make the users guess a list
-    word = list(word)                               # Make the selected word a list
-    position = 0                                    # Could be removed 
-    output = [" "] * 5                              # Output list to show hints
+    user_word = list(user_word)
+    word = list(word)
+    output = [" "] * 5
 
     for position in range(len(word)):
         if user_word[position] == word[position]:
-            output[position] = "X"                  # Updates the output list at the position where the letter was both in the word and correct position 
+            output[position] = "X"
         elif user_word[position] in word and word.index(user_word[position]) != position:
-            output[position] = "*"                  # Updates the output list at the position where the letter is in the word but not that position 
+            output[position] = "*"
         else:
-            output[position] = "-"                  # Updates the output list at the position indicating there's no occurrence of this letter in the selected word
+            output[position] = "-"
 
-    # Some nice colours and printing of the output
+    # Display the output with appropriate colors
     for position in range(len(output)):
         if output[position] == "X":
-            print(Fore.GREEN, output[position])
+            print(Fore.GREEN + output[position], end=" ")
         elif output[position] == "*":
-            print(Fore.YELLOW, output[position])
+            print(Fore.YELLOW + output[position], end=" ")
         else:
-            print(Fore.RED, output[position])
-            
-    # Determines if the user one or lost
+            print(Fore.RED + output[position], end=" ")
+
+    print(Style.RESET_ALL)
+
+    # Determine if the user won or lost
     if user_word == word:
         if tries == 1:
-            print("You won with one try remaining. That was close!")
+            print(Fore.GREEN + "You won with one try remaining. That was close!")
             tries = 1
 
         if cheat:
-            print(Fore.GREEN + "You win, but you cheated so you don't deserve it", Style.RESET_ALL)
-            tries = 0                               # Makes score 0 because of cheating
+            print(Fore.GREEN + "You win, but you cheated so you don't deserve it" + Style.RESET_ALL)
+            tries = 0
         else:
-            print(Fore.GREEN + "You win", Style.RESET_ALL)
+            print(Fore.GREEN + "You win" + Style.RESET_ALL)
 
         if config.get("upload_score", False) and tries != 0:
-            client.info_input(tries_remaining = tries) # If configuration is set to true attempt to upload scores
+            client.info_input(tries_remaining=tries)
     else:
-        tries -= 1  # Decrease the tries counter
+        tries -= 1
         if tries > 0:
-            print(Fore.MAGENTA + "Try again")
-            print(tries, "Tries remaining")
-            user_input(word, cheat, config, tries)  # Indicate the need for another input
+            print(Fore.MAGENTA + "Try again. " + str(tries) + " tries remaining.")
         else:
-            print(Fore.RED + "You lose")
+            print(Fore.RED + "You lost. Better luck next time!")
             if config.get("show_word_after_loss", False):
-                print("The word was:", " ".join(word)) # If configuration set to true show what the correct word was
+                print("The word was:", " ".join(word))
 
-# User Input
+    return tries
+
+# User input
 def user_input(word, cheat, config, tries):
     if cheat:
         print("Word:", word)
-        print("What's the fun in this") # Shows user is cheating 
+        print("What's the fun in this")                            # Shows user is cheating
 
-    user_word = input(Fore.CYAN + "Enter a 5-letter word: " + Style.RESET_ALL) # Asks for user input 
-    user_word = user_word.lower() # Makes users input lowercase 
+    user_word = input(Fore.CYAN + "Enter a 5-letter word: " + Style.RESET_ALL)    # Asks for user input
+    user_word = user_word.lower()                               # Makes users input lowercase
 
-    valid_words_contents = load_files()[1] # Calls the valid words from the returned tuple from the load_files function
+    valid_words_contents = load_files()[1]                       # Calls the valid words from the returned tuple from the load_files function
 
-    if len(user_word) == 5: # Checks the user inputted a 5 letter word as asked
+    if len(user_word) == 5:                                      # Checks if the user inputted a 5-letter word as asked
         if user_word in valid_words_contents:
             return algorithm(user_word, word, tries, config, cheat)
         else:
             print(Fore.RED + "Invalid word")
             user_input(word, cheat, config, tries)
     else:
-        print(Fore.RED + "Must be a 5 letter word. Try again.")
+        print(Fore.RED + "Must be a 5-letter word. Try again.")
         user_input(word, cheat, config, tries)
 
 def end():
@@ -136,21 +138,18 @@ def main(cheat):
 
     user_input(word, cheat, config, tries)
 
-import subprocess
-
 def show_instructions():
     file_path = 'Instructions.md'
     try:
-        subprocess.run(['open', file_path])  # For macOS
+        subprocess.run(['open', file_path])                       # For macOS
     except FileNotFoundError:
         try:
-            subprocess.run(['xdg-open', file_path])  # For Linux
+            subprocess.run(['xdg-open', file_path])               # For Linux
         except FileNotFoundError:
             try:
                 subprocess.run(['start', file_path], shell=True)  # For Windows
             except FileNotFoundError:
                 print("Unable to open the instructions file. Please refer to the README for instructions.")
-
 
 credits = """
     Main Developer: Kai Jane (kaijanedev@icloud.com)
@@ -182,7 +181,7 @@ while True:
         case 4:
             config = options_module.options()
         case 5:
-            print(Fore.YELLOW, credits)
+            print(Fore.YELLOW + credits)
             credit_input = input("Enter to continue: ")
             if check_konami_code(credit_input):
                 cheat = True
@@ -195,3 +194,4 @@ while True:
 
     if KeyboardInterrupt:
         cheat = False
+        continue
