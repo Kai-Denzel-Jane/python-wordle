4f49d737dd0814d2ec512e7002d20a3e4f69bb87
Kai Jane <120575219+Kai-Denzel-Jane@users.noreply.github.com>
2023-09-15 07:29:58 +0000

    commit

--- base.py
+++ base.py
@@ -7,6 +7,7 @@ app = Flask(__name__, static_folder="static", static_url_path="/static")
 
 app.secret_key = os.urandom(24)
 
+
 @app.route('/')
 def load_menu():
 
@@ -21,31 +22,41 @@ def process_option():
     if request.method == 'POST':
         selected_option = request.form.get('choice')
 
-
     if selected_option == "play":
         return redirect(url_for('flask_select_word'))
-    
+
     if selected_option is None:
         return "Invalid option or method used."
 
-@app.route('/play', methods=['GET', 'POST'])
 
+@app.route('/play', methods=['GET', 'POST'])
 def flask_select_word():
     word = session.get('word')
-
-    tries = MAX_TRIES
 
     if word is None:
         word = select_word()
         session['word'] = word
 
+    tries = session.get('tries')
+
+    if tries is None:
+        tries = 6
+        session['tries'] = tries
+
     # Check if there's a POST request
     if request.method == 'POST':
-        return user_input(word, cheat=False, config={}, tries)  # If it's a POST request, redirect to the user_input function
 
-    return render_template('game_main.html', word=word)  # If it's a GET request, render the template
+        tries = request.form.get('tries_left')
+        tries = int(tries)
 
-def user_input(word, cheat=False, config={}, tries=):
+        # If it's a POST request, redirect to the user_input function
+        user_input(word, tries, cheat=False, config={})
+
+    # If it's a GET request, render the template
+    return render_template('game_main.html', word=word, tries_left=tries)
+
+
+def user_input(word, tries, cheat=False, config={}):
     # if cheat:
     #     # ...
 
@@ -58,25 +69,28 @@ def user_input(word, cheat=False, config={}, tries=):
         if len(user_word) == 5:
             if user_word in valid_words_contents:
                 print("Valid word")
-                return flask_algorithm(user_word, word, tries, config, cheat)
+                flask_algorithm(user_word, word, tries, config, cheat)
             else:
                 print("Invalid word (not in dictionary)")
-                return render_template("game_main.html", word=session['word'], error_message="Invalid word (not in dictionary)", tries_remaining=tries)  # Pass updated tries value
+                # Pass updated tries value
+                return render_template("game_main.html", word=session['word'], error_message="Invalid word (not in dictionary)", tries_remaining=tries)
         else:
             print("Invalid word (not 5 letters)")
-            return render_template("game_main.html", word=session['word'], error_message="Invalid word (not 5 letters)", tries_remaining=tries)  # Pass updated tries value
+            # Pass updated tries value
+            return render_template("game_main.html", word=session['word'], error_message="Invalid word (not 5 letters)", tries_remaining=tries)
+
 
 def flask_algorithm(user_word, word, tries, config={}, cheat=False):
     """Main logic, this is where the scoring is implemented 
-    
-    
+
+
     Parameters:
         user_word (str): The word the user is guessing
         selected_word (str): The word that the user is guessing
         tries (int): The amount of tries the user has
         config (dict): The configuration file
         cheat (bool): Whether or not to load the game in cheat mode.
-    
+
     Returns:
         None    
     """
@@ -86,7 +100,8 @@ def flask_algorithm(user_word, word, tries, config={}, cheat=False):
     position = 0  # Could be removed
     output = [" "] * len(word)  # Output list to show hints
 
-    selected_word_counts = {}  # Dictionary to store the counts of each letter in the selected word
+    # Dictionary to store the counts of each letter in the selected word
+    selected_word_counts = {}
     for letter in word:
         if letter in selected_word_counts:
             selected_word_counts[letter] += 1
@@ -95,21 +110,27 @@ def flask_algorithm(user_word, word, tries, config={}, cheat=False):
 
     for position in range(len(word)):
         if user_word[position] == word[position]:
-            output[position] = "X"  # Updates the output list at the position where the letter was both in the word and correct position
-            selected_word_counts[user_word[position]] -= 1  # Decrease the count for correctly guessed letters
+            # Updates the output list at the position where the letter was both in the word and correct position
+            output[position] = "X"
+            # Decrease the count for correctly guessed letters
+            selected_word_counts[user_word[position]] -= 1
         elif user_word[position] in selected_word_counts and selected_word_counts[user_word[position]] > 0:
-            output[position] = "*"  # Updates the output list at the position where the letter is in the word but not at that position
-            selected_word_counts[user_word[position]] -= 1  # Decrease the count for partially correct letters
+            # Updates the output list at the position where the letter is in the word but not at that position
+            output[position] = "*"
+            # Decrease the count for partially correct letters
+            selected_word_counts[user_word[position]] -= 1
         else:
-            output[position] = "-"  # Updates the output list at the position where the letter is not in the word
-
+            # Updates the output list at the position where the letter is not in the word
+            output[position] = "-"
 
     # Some nice colors and printing of the output
     for position in range(len(output)):
         if output[position] == f"X":
-            print(Fore.WHITE + user_word[position], Fore.GREEN, output[position])
+            print(Fore.WHITE + user_word[position],
+                  Fore.GREEN, output[position])
         elif output[position] == f"*":
-            print(Fore.WHITE + user_word[position], Fore.YELLOW, output[position])
+            print(Fore.WHITE + user_word[position],
+                  Fore.YELLOW, output[position])
         else:
             print(Fore.WHITE + user_word[position], Fore.RED, output[position])
 
@@ -120,27 +141,30 @@ def flask_algorithm(user_word, word, tries, config={}, cheat=False):
             tries = 1
 
         if cheat:
-            print(Fore.GREEN + f"You win, but you cheated so you don't deserve it", Style.RESET_ALL)
+            print(
+                Fore.GREEN + f"You win, but you cheated so you don't deserve it", Style.RESET_ALL)
             tries = 0  # Makes score 0 because of cheating
         else:
             print(Fore.GREEN + f"You win", Style.RESET_ALL)
 
         if config.get(f"upload_score", False) and tries != 0:
-            client.info_input(tries_remaining=tries)  # If configuration is set to true, attempt to upload scores
+            # If configuration is set to true, attempt to upload scores
+            client.info_input(tries_remaining=tries)
     else:
         tries -= 1  # Decrease the tries counter
         if tries > 0:
             print(Fore.MAGENTA + f"Try again")
             print(tries, f"Tries remaining")
-            return user_input(word, cheat=False, config={}, tries=tries)  # Indicate the need for another input
+            print(Style.RESET_ALL)
+            session['tries'] = tries
+            # Pass updated tries value
+            return render_template("game_main.html", word=session['word'], error_message="Try again", tries_left=tries)
         else:
             print(Fore.RED + f"You lose")
             if config.get(f"show_word_after_loss", False):
-                print(f"The word was:", " ".join(word))  # If configuration set to true, show what the correct word was
-    
+                # If configuration set to true, show what the correct word was
+                print(f"The word was:", " ".join(word))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
-    
-
-
-
+    app.run(debug=True, port=5001)
